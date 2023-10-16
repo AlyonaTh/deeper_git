3a996af652c22689f5ffb674a4f4df73d687e3d0
Alyona <tyoaa51@gmail.com>
2023-10-16 21:11:29 +0600

    Развёртывание проекта

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,17 +20,39 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iuwe5-7^5*hwfj02h@4e@*pf8^r7_z*zy6syc9+gann212)o99'
+#SECRET_KEY = 'django-insecure-iuwe5-7^5*hwfj02h@4e@*pf8^r7_z*zy6syc9+gann212)o99'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.9.49']
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+STATIC_ROOT = BASE_DIR / 'static/'
+
+
+
+ALLOWED_HOSTS = [
+    '192.168.9.49'
+    '127.0.0.1',
+    'alyonath.pythonanywhere.com',
+]
+
+
 
 
 # Application definition
 
+INTERNAL_IPS = [
+    '192.168.0.105',
+    '127.0.0.1',
+]
+
+
 INSTALLED_APPS = [
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +66,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -79,8 +102,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Alyonath$default',
+        'USER': 'Alyonath',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'Alyonath.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';"
+                            "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
